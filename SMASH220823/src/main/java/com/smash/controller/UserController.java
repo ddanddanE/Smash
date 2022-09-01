bfa97e38e595025b862a1a4fc8e37708ffe6936f
DDanDDanE <speryon90@naver.com>
2022-09-01 17:02:46 +0900

    테스트_윤진환

--- SMASH220823/src/main/java/com/smash/controller/UserController.java
+++ SMASH220823/src/main/java/com/smash/controller/UserController.java
@@ -30,13 +30,13 @@ public class UserController {
 		return "/user/login";
 	}
 
-	// 메인페이지에서 로그인 관련 jsp열도록 하는 페이지
+	// 硫붿씤�럹�씠吏��뿉�꽌 濡쒓렇�씤 愿��젴 jsp�뿴�룄濡� �븯�뒗 �럹�씠吏�
 	@PostMapping("/login")
 	public String loginpost() {
 		return "/user/login";
 	}
 
-	// 로그인 페이지에서 아이디 비번 치고 맞으면 메인 아니면 다시 여기 페이지
+	// 濡쒓렇�씤 �럹�씠吏��뿉�꽌 �븘�씠�뵒 鍮꾨쾲 移섍퀬 留욎쑝硫� 硫붿씤 �븘�땲硫� �떎�떆 �뿬湲� �럹�씠吏�
 	@PostMapping("/logincheck")
 	public String logincheck(HttpSession session, @RequestParam("User_Id") String User_Id, @RequestParam("User_Pw") String User_Pw) {
 		
@@ -49,13 +49,13 @@ public class UserController {
 		return "/user/test";
 	}
 
-	// 메인페이지에서 회원가입하면 정보제공 동의
+	// 硫붿씤�럹�씠吏��뿉�꽌 �쉶�썝媛��엯�븯硫� �젙蹂댁젣怨� �룞�쓽
 	@GetMapping("/signup/agree")
 	public String agree() {
 		return "/user/agree";
 	}
 
-	// 정보제공 동의에서 인적사항입력
+	// �젙蹂댁젣怨� �룞�쓽�뿉�꽌 �씤�쟻�궗�빆�엯�젰
 	@PostMapping("/signup")
 	public String signup(Model m, HttpServletRequest request) {
 		String resultagr = request.getParameter("resultagr");
@@ -82,5 +82,7 @@ public class UserController {
 		}
 
 		return "/user/success";
+		
+		//test
 	}
 }
