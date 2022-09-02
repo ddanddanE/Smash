999558342d85e1e79fbca1cdec220443456cc152
jyy <kanda8@nate.com>
2022-09-02 11:59:52 +0900

    post ->main 접속 연동

--- SMASH220823/src/main/java/com/smash/controller/HomeController.java
+++ SMASH220823/src/main/java/com/smash/controller/HomeController.java
@@ -4,20 +4,33 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.smash.VO.match.noticeBVO;
+import com.smash.VO.user.UserVO;
+import com.smash.service.user.UserService;
+
+import lombok.RequiredArgsConstructor;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 	
+	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	private final UserService uService;
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
@@ -47,6 +60,18 @@ public class HomeController {
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "/main";
+	}
+	
+	@PostMapping("/main")
+	public String rate(Model m,noticeBVO no,UserVO uo,HttpSession session) {
+		
+		
+		UserVO vo = (UserVO) session.getAttribute("user");
+		
+		session.setAttribute("user", uService.login(vo));
+		
+		return "main";
+		
 	}
 	
 }
